036d64cf451fbc053ca7a6ed3a1ae26c67b80543
xanderez17 <kevxanderez17@gmail.com>
2022-05-05 16:58:54 -0500

    Creación Clase Login-Rol
    Clases de Login y Roles

---
+++ src/main/java/com/apecs/backend/modelo/Login.java
@@ -0,0 +1,63 @@
+package com.apecs.backend.modelo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "login")
+public class Login {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long idLogin;
+	
+	private String username;
+	
+	private String password;
+	
+	@ManyToOne
+	@JoinColumn(name = "idUsuario")
+	private RolUsuario rolUsuario;
+
+	public Login() {
+		super();
+	}
+
+	public Long getIdLogin() {
+		return idLogin;
+	}
+
+	public void setIdLogin(Long idLogin) {
+		this.idLogin = idLogin;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public RolUsuario getRolUsuario() {
+		return rolUsuario;
+	}
+
+	public void setRolUsuario(RolUsuario rolUsuario) {
+		this.rolUsuario = rolUsuario;
+	}
+	
+}
---
+++ src/main/java/com/apecs/backend/modelo/RolUsuario.java
@@ -0,0 +1,46 @@
+package com.apecs.backend.modelo;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name= "roles")
+public class RolUsuario {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long idUsuario;
+	
+	@Column(nullable = false)
+	private String rol;
+	
+	
+
+	public RolUsuario() {
+		super();
+	}
+
+	public Long getIdUsuario() {
+		return idUsuario;
+	}
+
+	public void setIdUsuario(Long idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+
+	public String getRol() {
+		return rol;
+	}
+
+	public void setRol(String rol) {
+		this.rol = rol;
+	}
+	
+	
+	
+
+}
